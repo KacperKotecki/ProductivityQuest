914c96283b54b00a1026632dac1cbfb8c9536295
Kacper Kotecki <kacperkotecki@o2.pl>
2025-05-06 07:41:58 +0200

    Refactor ChangeImg: use LoadFromResources and improve ghost selection logic

--- StatsRefresher.cs
+++ StatsRefresher.cs
@@ -57,72 +57,45 @@ namespace Productivity_Quest_1._0
 
         }
 
-        
-        public System.Drawing.Bitmap ChangeImg(int streak)
+
+        public Bitmap ChangeImg(int streak)
         {
-            if (streak < 0 && streak > -7)
-                streak = -1;
-            else
-                streak = streak / 7;
-
-            var happy_ghost = new List<Bitmap>
+            var happy_ghost = new List<Bitmap>();
+            for (int i = 0; i < 10; i++)
             {
-                Properties.Resources.happy_ghost_0,
-                Properties.Resources.happy_ghost_1,
-                Properties.Resources.happy_ghost_2,
-                Properties.Resources.happy_ghost_3,
-                Properties.Resources.happy_ghost_4,
-                Properties.Resources.happy_ghost_5,
-                Properties.Resources.happy_ghost_6,
-                Properties.Resources.happy_ghost_7,
-                Properties.Resources.happy_ghost_8,
-                Properties.Resources.happy_ghost_9,
-            };
-            var sad_ghost = new List<Bitmap>
-            {
-                Properties.Resources.sad_ghost_0,
-                Properties.Resources.sad_ghost_1,
-                Properties.Resources.sad_ghost_2,
-                Properties.Resources.sad_ghost_3,
-                Properties.Resources.sad_ghost_4,
-                Properties.Resources.sad_ghost_5,
-                Properties.Resources.sad_ghost_6,
-                Properties.Resources.sad_ghost_7,
-                Properties.Resources.sad_ghost_8,
-                Properties.Resources.sad_ghost_9,
-
-
-            };
-            System.Drawing.Bitmap img = Properties.Resources.happy_ghost_0;
-
-            switch (streak)
-            {
-
-                case -10: img = sad_ghost[9]; break;
-                case -9: img = sad_ghost[8]; break;
-                case -8: img = sad_ghost[7]; break;
-                case -7: img = sad_ghost[6]; break;
-                case -6: img = sad_ghost[5]; break;
-                case -5: img = sad_ghost[4]; break;
-                case -4: img = sad_ghost[3]; break;
-                case -3: img = sad_ghost[2]; break;
-                case -2: img = sad_ghost[1]; break;
-                case -1: img = sad_ghost[0]; break;
-                case 0: img = happy_ghost[0]; break;
-                case 1: img = happy_ghost[1]; break;
-                case 2: img = happy_ghost[2]; break;
-                case 3: img = happy_ghost[3]; break;
-                case 4: img = happy_ghost[4]; break;
-                case 5: img = happy_ghost[5]; break;
-                case 6: img = happy_ghost[6]; break;
-                case 7: img = happy_ghost[7]; break;
-                case 8: img = happy_ghost[8]; break;
-                case 9: img = happy_ghost[9]; break;
-
-
+                string name = $"happy_ghost_{i}";
+                Bitmap img = (Bitmap)Properties.Resources.ResourceManager.GetObject(name);
+                if (img != null)
+                    happy_ghost.Add(img);
             }
 
-            return img;
+            var sad_ghost = new List<Bitmap>();
+            for (int i = 0; i < 10; i++)
+            {
+                string name = $"sad_ghost_{i}";
+                Bitmap img = (Bitmap)Properties.Resources.ResourceManager.GetObject(name);
+                if (img != null)
+                    sad_ghost.Add(img);
+            }
+
+            Bitmap imgResult = happy_ghost[0]; // default
+
+            int index = streak / 7;
+
+            if (streak > 0)
+            {
+                imgResult = happy_ghost[Math.Min(index, happy_ghost.Count - 1)];
+            }
+            else if (streak < 0)
+            {
+                imgResult = sad_ghost[Math.Min(Math.Abs(index), sad_ghost.Count - 1)];
+            }
+            else
+            {
+                imgResult = happy_ghost[0];
+            }
+
+            return imgResult;
         }
 
     }
