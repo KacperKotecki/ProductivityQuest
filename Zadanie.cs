65729f33af600dd305b1d55eeed29b1e3a99a5e7
Kacper Kotecki <kacperkotecki@o2.pl>
2025-05-17 15:55:27 +0200

    Add universal Id field to Zadanie.cs for task identity

--- Zadanie.cs
+++ Zadanie.cs
@@ -5,6 +5,7 @@ namespace Productivity_Quest_1._0
 {
     public class Zadanie
     {
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Category { get; set; }
         public string Priority { get; set; }
