c868854a8e253587f83d928dfc09916b63784be8
Kacper Kotecki <kacperkotecki@o2.pl>
2025-10-28 00:33:55 +0100

    refactor: Code fixes and compilation error resolution
    Removed unnecessary ‘using’ directive that caused a compilation error due to ambiguity of the ‘Image’ type. Also fixed method call to remove type conversion errors.

--- TaskPanelBuilder.cs
+++ TaskPanelBuilder.cs
@@ -2,7 +2,6 @@
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Productivity_Quest_1._0
 {
@@ -220,22 +219,9 @@ namespace Productivity_Quest_1._0
                 Image = image,
                 //Margin = new Padding(2),
                 //Dock = DockStyle.Top
-
-            return icon;
-        }
-        public PictureBox CreateIconPictureBox(string iconPath, Size size, Point location)
-        {
-            var icon = new PictureBox
-            {
-                Size = size,
-                Location = location,
-                SizeMode = PictureBoxSizeMode.Zoom,
-                BorderStyle = BorderStyle.None,
-                BackColor = Color.Transparent,
-                Image = System.Drawing.Image.FromFile(iconPath)
             };
-
             return icon;
         }
+        
     }
 }
